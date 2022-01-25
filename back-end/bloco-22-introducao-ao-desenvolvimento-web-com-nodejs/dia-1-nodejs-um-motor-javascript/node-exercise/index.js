499984358ddf44cf578662aca9a262edf7070860
paulohp7 <ph.pereira.7@gmail.com>
2022-01-25 10:15:34 -0300

    Implemento IMC e exporto para o index.js

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/node-exercise/index.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/node-exercise/index.js
@@ -1,4 +1,8 @@
 const readline = require('readline-sync');
+const CalcIMC = require('./scripts/imc')
 const fileSystem = require('fs');
-const nome = readline.question('Qual o seu nome? ');
-console.log(`Oi ${nome}`);
+const name = readline.question('What is your name? ');
+const height = readline.questionFloat('What is your height? ');
+const weight = readline.questionFloat('What is your weight? ', { hideEchoBack: true });
+console.log(`Hi ${name}`);
+console.log(CalcIMC.imc(weight, height));
