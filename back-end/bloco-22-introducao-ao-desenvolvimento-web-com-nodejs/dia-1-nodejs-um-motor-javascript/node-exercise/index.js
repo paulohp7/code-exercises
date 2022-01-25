c4cb5b4e01e50b50c9ca74c89c8ab907fd9f68e6
paulohp7 <ph.pereira.7@gmail.com>
2022-01-25 16:59:18 -0300

    Faço a classificacao do Body Mass Index(IBM) da pessoa

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/node-exercise/index.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/node-exercise/index.js
@@ -1,8 +1,14 @@
 const readline = require('readline-sync');
 const CalcIMC = require('./scripts/imc')
-const fileSystem = require('fs');
 const name = readline.question('What is your name? ');
 const height = readline.questionFloat('What is your height? ');
 const weight = readline.questionFloat('What is your weight? ', { hideEchoBack: true });
 console.log(`Hi ${name}`);
-console.log(CalcIMC.imc(weight, height));
+const BMI = +CalcIMC(weight, height).toFixed(1);
+console.log(`Your BMI is: ${BMI}`);
+if (BMI < 18.5) console.log('Abaixo do peso (magreza)');
+else if (BMI >= 18.5 && BMI < 25) console.log('Peso normal');
+else if (BMI >= 25 && BMI < 30) console.log('Acima do peso (sobrepeso)');
+else if (BMI >= 30 && BMI < 35) console.log('Obesidade grau I');
+else if (BMI >= 35 && BMI < 40) console.log('Obesidade grau II');
+else console.log('Obesidade graus III e IV');
--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/node-exercise/scripts/imc.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/node-exercise/scripts/imc.js
@@ -1,3 +1,3 @@
 const imc = (weight, height) => weight / (height ** 2);
 
-module.exports = { imc };
+module.exports = imc;
