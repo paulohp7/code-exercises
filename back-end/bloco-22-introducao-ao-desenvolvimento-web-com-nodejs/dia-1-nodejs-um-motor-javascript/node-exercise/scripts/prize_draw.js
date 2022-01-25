08903e3dd7382a06c1955cf66eef679305737f31
paulohp7 <ph.pereira.7@gmail.com>
2022-01-25 18:14:42 -0300

    prize_draw.js new features (you can repeat the game if you want it)

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/node-exercise/scripts/prize_draw.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/node-exercise/scripts/prize_draw.js
@@ -7,7 +7,14 @@ function getRandomIntInclusive(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
 }
 
-const drawNumber = getRandomIntInclusive(0, 10);
-const chosenNumber = readline.question('Which number will you choose? ');
-if (drawNumber == chosenNumber) console.log('Congratulations! Correct number!');
-else console.log(`Sorry, maybe next time! The draw number was ${drawNumber}`);
+function runGame() {
+    const drawNumber = getRandomIntInclusive(0, 10);
+    const chosenNumber = readline.question('Which number will you choose? ');
+    if (drawNumber == chosenNumber) console.log(`Congratulations! Your number ${chosenNumber} is the same draw number ${drawNumber}!`);
+    else console.log(`Sorry, maybe next time! The draw number was ${drawNumber}`);
+    const repeat = readline.question('Do you wanna play again? Y or N ');
+    if (repeat !== 'Y') return console.log('See you next time!');
+    runGame();
+}
+
+runGame();
