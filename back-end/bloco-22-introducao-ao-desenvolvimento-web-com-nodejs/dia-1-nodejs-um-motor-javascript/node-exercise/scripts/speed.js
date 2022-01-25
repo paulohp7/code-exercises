2b1fe51caa2bfd40610388aa9d32fee42b9af73d
paulohp7 <ph.pereira.7@gmail.com>
2022-01-25 17:18:31 -0300

    Speed.js implemented, script in package.json translated to npm run speed

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/node-exercise/scripts/speed.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/node-exercise/scripts/speed.js
@@ -1,1 +1,6 @@
 const readline = require('readline-sync');
+
+const distance = readline.questionInt('What is the distance in meters? ');
+const seconds = readline.questionInt('How many seconds? ');
+const speed = distance / seconds;
+console.log(`Your avarage speed is: ${speed} meters per second`);
