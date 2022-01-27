1915ad3fba8e833c3d2c0c8ce546382ad84da5fa
paulohp7 <ph.pereira.7@gmail.com>
2022-01-27 18:14:41 -0300

    Implemented the asynchronous function myNumbers

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-asynchronous-flux/scripts/promises.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-asynchronous-flux/scripts/promises.js
@@ -1,1 +1,20 @@
 const readline = require('readline-sync');
+
+const paramA = readline.questionFloat('Write a number: ');
+const paramB = readline.questionFloat('Write another number: ');
+const paramC = readline.questionFloat('Write another number: ');
+const numbers = [paramA, paramB, paramC];
+
+function myNumbers(numbers) {
+    const promise = new Promise((resolve, reject) => {
+        if (numbers.find((number) => typeof number !== Number)) reject(new Error('Informe apenas números'))
+        const myOperation = (numbers[0] + numbers[1]) * numbers[2];
+        if (myOperation < 50) reject(new Error('Valor muito baixo')) 
+        resolve(myOperation)
+    });
+    return promise;
+  }
+  
+  /*dividirNumeros(2, 1)
+    .then(result => console.log(`sucesso: ${result}`))
+    .catch(err => console.log(`erro: ${err.message}`));*/
