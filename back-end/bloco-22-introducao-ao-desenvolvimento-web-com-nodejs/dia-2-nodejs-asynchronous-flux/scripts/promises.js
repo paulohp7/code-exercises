37ea5cf3da1ce380f6425fcb2c9800e2959c6b7d
paulohp7 <ph.pereira.7@gmail.com>
2022-01-27 18:41:58 -0300

    Fixed the function myNumbers, created gitignore and started npm init -y

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-asynchronous-flux/scripts/promises.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-asynchronous-flux/scripts/promises.js
@@ -1,19 +1,21 @@
 const readline = require('readline-sync');
 
-const paramA = readline.questionFloat('Write a number: ');
-const paramB = readline.questionFloat('Write another number: ');
-const paramC = readline.questionFloat('Write another number: ');
-const numbers = [paramA, paramB, paramC];
-
 function myNumbers(numbers) {
     const promise = new Promise((resolve, reject) => {
-        if (numbers.find((number) => typeof number !== Number)) reject(new Error('Informe apenas números'))
+        if (numbers.some((number) => typeof number !== 'number')) reject(new Error('Informe apenas números'))
         const myOperation = (numbers[0] + numbers[1]) * numbers[2];
         if (myOperation < 50) reject(new Error('Valor muito baixo')) 
         resolve(myOperation)
     });
     return promise;
   }
+
+const paramA = readline.question('Write a number: ');
+const paramB = readline.question('Write another number: ');
+const paramC = readline.question('Write another number: ');
+const numbers = [paramA, paramB, paramC];
+
+myNumbers(numbers);
   
   /*dividirNumeros(2, 1)
     .then(result => console.log(`sucesso: ${result}`))
