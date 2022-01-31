b537ac9bcdec315b36842fbb39b9d493c1d55e14
paulohp7 <ph.pereira.7@gmail.com>
2022-01-30 21:48:09 -0300

    Exercise two

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/day-4-node-with-http-express/index.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/day-4-node-with-http-express/index.js
@@ -1,13 +1,18 @@
 const express = require('express');
-// const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 
 const app = express();
-// app.use(bodyParser.json());
+app.use(bodyParser.json());
 
 
 app.get('/ping', function (req, res) {
   res.status(200).json({ "message": 'pong' });
 });
+
+app.post('/hello', function (req, res) {
+    const { name } = req.body;
+    res.status(201).json({ message:`Hello ${name}!` });
+  });
 
 // app.get('/recipes/pesquisar', function (req, res) {
 //   const { name, maxPrice } = req.query;
@@ -23,11 +28,7 @@ app.get('/ping', function (req, res) {
 //   res.status(200).json(recipe);
 // });
 
-// app.post('/recipes', function (req, res) {
-//   const { id, name, price } = req.body;
-//   recipes.push({ id, name, price});
-//   res.status(201).json({ message: 'Recipe created successfully!'});
-// });
+
 
 // app.put('/recipes/:id', function (req, res) {
 //   const { id } = req.params;
