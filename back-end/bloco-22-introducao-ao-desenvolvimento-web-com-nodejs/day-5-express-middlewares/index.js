113b03f5f8d6bb5d00c9133904349a3122c96949
paulohp7 <ph.pereira.7@gmail.com>
2022-02-01 17:03:32 -0300

    Exercise two

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/day-5-express-middlewares/index.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/day-5-express-middlewares/index.js
@@ -1,34 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const routerUser = require('./routerUser');
 
 const app = express();
 app.use(bodyParser.json());
 
-const validateEmail = (email) => {
-  return String(email)
-    .toLowerCase()
-    .match(
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    );
-};
-
-const validatePassword = (password) => {
-  if (password.length > 3 && password.length < 9) {
-    return password.match(/^[0-9]*$/);
-  };
-  return false;
-}
-  
-const authMiddleware = (req, res, _next) => {
-  const { username, email, password } = req.body;
-  
-  if (username.length > 3 && validateEmail(email) && validatePassword(password)) {
-    res.status(201).json({ message: 'user created' });
-  } else res.status(400).json({ message: 'invalid data' });
-
-};
-
-app.post('/user/register', authMiddleware);
+app.use('/user', routerUser);
 
 app.listen(3000, () => {
   console.log('Listening the application on port 3000');
