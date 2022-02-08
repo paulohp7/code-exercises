1bf9d503b434116c0ade746f6998cb548b71e890
paulohp7 <ph.pereira.7@gmail.com>
2022-02-08 00:31:59 -0300

    Revert "Model Layer. First Day."

--- back-end/block-23-nodejs-service-layer-and-rest-restful-architecture/day-one-software-architecture-model-layer/models/connection.js
+++
@@ -1,9 +0,0 @@
-const mysql = require('mysql2/promise');
-
-const connection = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '12345678',
-    database: 'DB_users' });
-
-module.exports = connection;
