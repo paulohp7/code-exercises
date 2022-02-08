d0d85c46346259d50fb85717b79a31be423201c4
paulohp7 <ph.pereira.7@gmail.com>
2022-02-07 21:43:05 -0300

    Created post route with validations to insert an user in the database

--- back-end/block-23-nodejs-service-layer-and-rest-restful-architecture/day-one-software-architecture-model-layer/index.js
+++ back-end/block-23-nodejs-service-layer-and-rest-restful-architecture/day-one-software-architecture-model-layer/index.js
@@ -1,13 +1,28 @@
+const e = require('express');
 const express = require('express');
 const User = require('./models/Users');
 
 const app = express();
+app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
-app.get('/users', async (_req, res) => {
+app.get('/user', async (_req, res) => {
     const myUsers = await User.getAll();
     res.status(200).json(myUsers);
+});
+
+app.post('/user', async (req, res) => {
+    const { firstName, lastName, email, password } = req.body;
+    if (!firstName) res.status(400).json({ error: true, message: 'Name is mandatory' });
+    else if (!lastName) res.status(400).json({ error: true, message: 'Last Name is mandatory' });
+    else if (!email) res.status(400).json({ error: true, message: 'Email is mandatory' });
+    else if (!password) res.status(400).json({ error: true, message: 'Password is mandatory' });
+    else if (password.length < 6) res.status(400).json({ error: true, message: 'Password must have at least 6 digits' });
+    else {
+        const [metaData] = await User.create(firstName, lastName, email, password);
+        res.status(201).json({ id: metaData.insertId, ...req.body });
+    }
 });
 
 app.listen(PORT, () => {
--- back-end/block-23-nodejs-service-layer-and-rest-restful-architecture/day-one-software-architecture-model-layer/models/Users.js
+++ back-end/block-23-nodejs-service-layer-and-rest-restful-architecture/day-one-software-architecture-model-layer/models/Users.js
@@ -1,10 +1,25 @@
+const e = require('express');
 const connection = require('./connection');
 
+const serialize = (user) => ({
+    id: user.id,
+    firstName: user.first_name,
+    lastName: user.last_name,
+    email: user.email
+});
+
 const getAll = async () => {
-    const [users] = await connection.execute('SELECT * FROM DB_users.tb_users;');
-    return users;
+    const [users] = await connection.execute('SELECT id, first_name, last_name, email FROM DB_users.tb_users;');
+    return users.map(serialize);
 };
+
+const create = async (firstName, lastName, email, password) => {
+    const query = 'INSERT INTO DB_users.tb_users (first_name, last_name, email, user_password) VALUES (?, ?, ?, ?)';
+    const metaData = await connection.execute(query, [firstName, lastName, email, password]);
+    return metaData;
+}
 
 module.exports = {
     getAll,
+    create,
 };
