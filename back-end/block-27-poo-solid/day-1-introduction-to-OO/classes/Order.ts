f993faee3b03d8ad0215927b6e9ce5b9689ef0f7
paulohp7 <ph.pereira.7@gmail.com>
2022-03-03 18:44:17 -0300

    New methods to Order class: sumOrder and discountOrder

--- back-end/block-27-poo-solid/day-1-introduction-to-OO/classes/Order.ts
+++ back-end/block-27-poo-solid/day-1-introduction-to-OO/classes/Order.ts
@@ -6,7 +6,7 @@ export class Order {
     private _customer: Customer;
     private _items: Item[];
     private _paymentMethod: string;
-    private _percentageDiscount = 0;
+    private _percentageDiscount = 1;
     private _discount = false;
 
     constructor(customer: Customer, items: Item[], paymentMethod: string) {
@@ -15,6 +15,14 @@ export class Order {
         this._paymentMethod = paymentMethod;
     }
     
+    get sumOrder() : number {
+        return this._items.reduce((partialSum, a) => partialSum + a.price, 0);
+    }
+
+    get discountOrder() : number {
+        if (this._discount) return this.sumOrder * this._percentageDiscount;
+        else return this.sumOrder;
+    }
     // public get name() : string {
     //     return this._name
     // }
