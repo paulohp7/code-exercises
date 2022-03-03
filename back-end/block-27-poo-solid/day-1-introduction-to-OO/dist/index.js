f1b69121d9f36719a565a89df8be3de342b878ad
paulohp7 <ph.pereira.7@gmail.com>
2022-03-03 17:22:07 -0300

    Adding methods to calculate the student's avarage

--- back-end/block-27-poo-solid/day-1-introduction-to-OO/dist/index.js
+++ back-end/block-27-poo-solid/day-1-introduction-to-OO/dist/index.js
@@ -1,35 +1,22 @@
+"use strict";
 class Student {
-
-    private _enrollment: number;
-    private _name: string;
-    private _tests: number[];
-    private _projects: number[];
-
-    constructor(
-        enrollment: number,
-        name: string,
-        tests: number[],
-        projects: number[],
-    ) {
+    constructor(enrollment, name, tests, projects) {
         this._enrollment = enrollment;
         this._name = name;
         this._tests = tests;
         this._projects = projects;
     }
-
-    private sumGrades() : number {
+    sumGrades() {
         const testsSum = this._tests.reduce((partialSum, a) => partialSum + a, 0);
         const projectsSum = this._projects.reduce((partialSum, a) => partialSum + a, 0);
         return testsSum + projectsSum;
     }
-
-    avaregaGrades() : number {
-        const TOTAL_GRADES: number = 6;
+    avaregaGrades() {
+        const TOTAL_GRADES = 6;
         const avarage = this.sumGrades() / TOTAL_GRADES;
         return Number(avarage.toFixed(2));
     }
 }
-
 const student1 = new Student(1234, 'John', [10, 10, 10, 10], [10, 10]);
 console.log(`John avarage grade is: ${student1.avaregaGrades()}`);
 const student2 = new Student(4321, 'Nick', [10, 5, 9, 8], [8, 7]);
